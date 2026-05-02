18a073f6e97350ef8256ec0ba15eda47b8319738
ktanveersingh13-prog <ktanveersingh13@gmail.com>
2026-05-03 00:07:53 +0530

    MainActivity.kt

--- java/com/stark/superisland/MainActivity.kt
+++ java/com/stark/superisland/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.stark.superisland
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // This is a simple button to take you to the Notification Settings
-        val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
-        startContext(intent)
+        // Request Floating Window Permission
+        if (!Settings.canDrawOverlays(this)) {
+            startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")))
+        }
         
-        finish() // Closes the screen after opening settings
-    }
-
-    private fun startContext(intent: Intent) {
-        startActivity(intent)
+        // Request Notification Access
+        startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"))
+        
+        finish()
     }
 }
