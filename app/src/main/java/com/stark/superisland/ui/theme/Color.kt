8fc81a4f9752ed907a69b2cac451d2cd5eaf30d1
ktanveersingh13-prog <ktanveersingh13@gmail.com>
2026-05-02 23:07:16 +0530

    Create Color.kt

---
+++ app/src/main/java/com/stark/superisland/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.stark.superisland.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// STARK LAB PRIMARY COLORS
+val StarkWhite = Color(0xFFFFFFFF)        // Pure white base
+val StarkLabGray = Color(0xFFF5F5F7)      // Professional depth gray
+val StarkDeepBlack = Color(0xFF000000)    // True black for the Island
+
+// STARK TECH ACCENTS
+val StarkTechBlue = Color(0xFF00BFFF)     // Arc Reactor Blue
+val StarkEnergyRed = Color(0xFFFF3B30)    // Alert red
+val StarkSuccessGreen = Color(0xFF34C759) // Success green
