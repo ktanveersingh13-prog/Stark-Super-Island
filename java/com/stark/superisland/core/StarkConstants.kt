6456e5955c64fa1f3cff8b3a3efa32ebba4a4c33
ktanveersingh13-prog <ktanveersingh13@gmail.com>
2026-05-03 11:03:32 +0530

    StarkConstants.kt

---
+++ java/com/stark/superisland/core/StarkConstants.kt
@@ -0,0 +1,16 @@
+package com.stark.superisland.core
+
+object StarkConstants {
+    // Animation Settings
+    const val ANIM_DURATION_SHORT = 300L
+    const val ANIM_DURATION_LONG = 600L
+    const val BOUNCE_INTENSITY = 0.7f
+
+    // UI Colors (Stark Cyan & Iron Man Red)
+    const val STARK_CYAN = "#00FBFF"
+    const val IRON_RED = "#FF0000"
+
+    // Security Keys
+    const val PREF_NAME = "stark_secure_prefs"
+    const val KEY_IS_ACTIVE = "system_status"
+}
